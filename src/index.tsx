7c4e5edcfd9296c51590980ec044b48091d5c524
Trevor Nielsen <trevor.nielsen@gmail.com>
2020-08-06 15:43:41 -0700

    started adding PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Helmet } from 'react-helmet';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import theme from './theme';
-
-const TITLE = 'Laser Protocol Designer'
+import registerServiceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <Helmet>
-      <title>{ TITLE }</title>
-    </Helmet>
     <App />
   </ThemeProvider>,
   document.querySelector('#root'),
+  registerServiceWorker,
 );
