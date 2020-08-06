12983e3159892c90e6d004578d9584b65e63f9bf
Trevor Nielsen <trevor.nielsen@gmail.com>
2020-08-06 12:38:43 -0700

    Changed website title

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import theme from './theme';
+
+const TITLE = 'Laser Protocol Designer'
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
+    <Helmet>
+      <title>{ TITLE }</title>
+    </Helmet>
     <App />
   </ThemeProvider>,
   document.querySelector('#root'),
