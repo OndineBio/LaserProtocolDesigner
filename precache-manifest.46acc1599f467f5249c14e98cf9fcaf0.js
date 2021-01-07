ef84e7c82d7b022dda8efb7f28e53eaf94a0608a
ninjas28 <gooseleader@geese.wtf>
2021-01-07 20:12:06 +0000

    Deploying to gh-pages from  @ 80df1c5663fdfa5c589e4c4318d218b475c8811d 🚀

---
+++ precache-manifest.46acc1599f467f5249c14e98cf9fcaf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddf2f24628cddc83af5258c2e55d1a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "324ade72595dd4931bf8",
+    "url": "/static/js/2.635e8924.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/static/js/2.635e8924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f22d424d5b85634f83",
+    "url": "/static/js/main.29932c71.chunk.js"
+  },
+  {
+    "revision": "e9f832037d535b2456e6",
+    "url": "/static/js/runtime-main.869c47e7.js"
+  }
+]);
